b99a70fe8de3129370728afdaeea094d325b01ef
saikumarreddi <skmr37173@gmail.com>
2020-11-17 21:37:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-image-zoom-demo-j4msva
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-image-zoom-demo-j4msva)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  myThumbnail="https://wittlock.github.io/ngx-image-zoom/assets/thumb.jpg";
+  myFullresImage="https://wittlock.github.io/ngx-image-zoom/assets/fullres.jpg";
+}
